4eea7d277302f9497231990818f4f23f3ae1e95d
ManavJoshi-31 <manavjoshi313@gmail.com>
2026-08-22 00:03:14 +0530

    feat: connect backend to MongoDB Atlas

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,13 @@
+import "dotenv/config";
+console.log("MONGO_URI loaded:", !!process.env.MONGO_URI);
 import express from "express";
+import connectDB from "./config/db.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.use(express.json());
 
