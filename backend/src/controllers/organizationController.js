59d9c81d3bdef0bec511a08541d7bcaa3345e076
ManavJoshi-31 <manavjoshi313@gmail.com>
2026-09-14 11:58:00 +0530

    feat : add organization creation API

--- backend/src/controllers/organizationController.js
+++ backend/src/controllers/organizationController.js
@@ -1,30 +1,37 @@
 import Organization from "../models/Organization.js";
-export const createOrganization = async(req,res)=>{
-    try{
-        const { name,description}=req.body;
-        if(!name){
-            return res.status(400).json({
-                message :  "Organization name is required"
-            });
-        }
+export const createOrganization = async (req, res) => {
+  try {
+    const { name, email, address } = req.body;
 
-        const existingOrganization = await Organization.findOne({name});
-        if(existingOrganization){
-            return res.status(409).json({
-                message: "Organization already exists"
-            });
-        }
-        const organization = await Organization.create({
-            name,description
-        });
-        return res.status(201).json({
-            message : "Organization created successfully",
-            organization   
-        });
-    }catch(error){
-        console.error("Create organization error:", error.message);
-        return res.status(500).json({
-            message: "Internal server error"
-        });
+    if (!name || !email) {
+      return res.status(400).json({
+        message: "Organization name and email are required"
+      });
     }
+
+    const existingOrganization = await Organization.findOne({ email });
+
+    if (existingOrganization) {
+      return res.status(409).json({
+        message: "Organization with this email already exists"
+      });
+    }
+
+    const organization = await Organization.create({
+      name,
+      email,
+      address
+    });
+
+    return res.status(201).json({
+      message: "Organization created successfully",
+      organization
+    });
+  } catch (error) {
+    console.error("Create organization error:", error.message);
+
+    return res.status(500).json({
+      message: "Internal server error"
+    });
+  }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,16 @@
 import "dotenv/config";
 import express from "express";
 import connectDB from "./config/db.js";
-
+//import routes
+import authRoutes from "./auth/authRoute.js";
+import organizationRoutes from "./routes/organizationRoutes.js";
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 connectDB();
-
 app.use(express.json());
+app.use("/api/organizations", organizationRoutes);
 
 app.get("/api/test", (req, res) => {
   res.status(200).json({
@@ -20,3 +22,6 @@ app.get("/api/test", (req, res) => {
 app.listen(PORT, () => {
   console.log(`FlowLedger server running on port ${PORT}`);
 });
+
+//use for routing to authRoutes
+app.use("/api/auth", authRoutes);
